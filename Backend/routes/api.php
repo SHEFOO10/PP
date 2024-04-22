7e0d3a5ab5378b380b0795bdbe8046d51a28d814
Sherif Hamdy <445585913@g102019.moe.edu.eg>
2024-04-22 20:22:35 +0200

    User must verify Email

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -16,13 +16,17 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\AuthController;
 
-Route::group([
-    'middleware' => 'api',
-    'prefix' => 'auth'
-], function ($router) {
+
+Route::middleware('api')->prefix('auth')->group(function () {
     Route::post('register', [AuthController::class, 'register']);
     Route::post('login', [AuthController::class, 'login']);
     Route::post('logout', [AuthController::class, 'logout']);
     Route::post('refresh', [AuthController::class, 'refresh']);
     Route::post('me', [AuthController::class, 'me']);
+});
+
+
+Route::middleware('api')->prefix('email')->group(function () {
+    Route::get('/email/verify', function () {})->name('verification.notice');
+    Route::get('/email/verify', function () {})->name('verification.verify');
 });
